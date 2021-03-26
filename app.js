788036c16f26f9da9f03fcec5a84062eb2d7743d
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-26 15:50:39 -0400

    Implemented the check to test for result count for a given query.

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const fetchData = (query) => {
 
     axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=${NEWS_API_KEY}`)
         .then(response => {
-            console.log(response.data);
+
+            const { totalResults, articles } = response.data;
+
+            if (totalResults === 0) {
+                console.log(`No result found for the query : "${query}"`);
+            } else {
+                console.log(articles);
+            }
+
         }).catch(error => {
             console.log(`Error while fetching data : ${error}`);
         });
