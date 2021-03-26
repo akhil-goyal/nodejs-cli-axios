3b5fb28925323671877059a754789bc12e37ddb0
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-26 16:15:04 -0400

    Added the functionality to write the data to file.

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const NEWS_API_KEY = 'c3cd6edd23c64ae09998ad508e1076e8';
 
 let query = process.argv[2];
+
+const createFile = (articles) => {
+
+    let file = fs.createWriteStream('headlines.txt');
+
+    file.on('error', (error) => { console.log(`Error while creating file : ${error}`) });
+
+    articles.map((article, index) => { file.write(`${index + 1}. ${article.title} \n\n`); });
+
+    file.end();
+
+}
 
 const fetchData = (query) => {
 
@@ -15,6 +28,7 @@ const fetchData = (query) => {
                 console.log(`No result found for the query : "${query}"`);
             } else {
                 console.log(articles);
+                createFile(articles);
             }
 
         }).catch(error => {
